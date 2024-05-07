5ea75cdc69359c90bb3c726b3e451c75e4b70150
Flareon <167229955+xyzkyle@users.noreply.github.com>
2024-05-07 16:30:37 +0500

    Create .lua

---
+++ .lua
@@ -0,0 +1,24 @@
+local F = [[ Protected with LuaCat ]];
+local u = 'Welcome to LuaCat _ 1.20.12 ALpha Update : By Now at flarex.w3spaces.com/LuaCat'
+local c = 'x[]E[e]x[EXDExe[xExe/X?Ex[]e/e[/EX][/x[/ex][/edx[]ed/x][edx/ed][x/[]d/x4][ed/ec][e/{#?D#e[]?$R}?DCEX}{?';
+local k = 'Iam too scared to show up' [ 348 * HUE == 3] 'MainMenu.so';local F = [[ FlareX (AntiCheat) = '';]];
+local E = '[Decrypted by do you Know who i am?]';
+return(function(...)local --[[ ec d]] e = Create:GMMod();end(...);
+Game={}
+  function Game:init()
+    os.execute("sleep 5")
+    local r = {"functions"}
+  io.open(".lua")
+    dofile("/xyzkyle/disabler/.lua"
+    end
+  end
+end
+return(Game)
+Return os
+return Game
+local Flarex={notes}
+function Flarex:init(notes);
+  print(notes.ip={"?SERVER/?CLIENT/?security?/ip/?clients/?LCOM??/%%client%%/?IPs?/%%serverip.txt"})
+  print(notes.errors={"%%localappdata%%/Flarex/errors/er.txt"})
+end
+return Flarex
